02d73fd86da13a6758dbb3527288c0c69fcf2b3d
ThanaphonLeonardi <thanaphon.leonardi@gmail.com>
2022-07-07 13:25:37 +0200

    Fixed run_simulation.sh

--- run_simulation.sh
+++ run_simulation.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+# example: ./run_simulation.sh parking true
+# example: ./run_simulation.sh street false
+
 # Make sure that the .yaml and .pgm files are in the nav2_bringup/maps and
 # nav2_bringup/worlds directories
-# example: ./run_simulation.sh world.yaml world.model true
-# param1: .yaml file of the mapped environment
-# param2: .model file of the world
-# param3: true to RUN simulaton, false to build only
+# param1: name of the environment (e.g. "parking" automtically becomes "map_parking.yaml", "world_parking.model")
+# param2: true to run simulaton, false to build only
 
 # Modify launch file map and world names
 cd navigation2/nav2_bringup/bringup/launch/
-awk '{gsub(/map+\_+[a-z+0-9]+\.+yaml/, "'$1'")}1' tb3_simulation_launch.py > tb3_simulation_launch_new.py
+awk '{gsub(/map+\_+[a-z+0-9]+\.+yaml/, "map_'$1'.yaml")}1' tb3_simulation_launch.py > tb3_simulation_launch_new.py
 mv tb3_simulation_launch_new.py tb3_simulation_launch.py
-awk '{gsub(/world+\_+[a-z+0-9]+\.+model/, "'$2'")}1' tb3_simulation_launch.py > tb3_simulation_launch_new.py
+awk '{gsub(/world+\_+[a-z+0-9]+\.+model/, "world_'$1'.model")}1' tb3_simulation_launch.py > tb3_simulation_launch_new.py
 mv tb3_simulation_launch_new.py tb3_simulation_launch.py
 
 # Build the simulation
@@ -23,6 +24,6 @@ colcon build --packages-select-by-dep nav2_bringup
 source ./install/setup.bash
 
 # Run it!
-if ($3 == true) then
+if ($2 == true) then
   ros2 launch nav2_bringup tb3_simulation_launch.py
 fi
